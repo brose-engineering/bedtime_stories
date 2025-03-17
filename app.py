d425991fe6c94150c974d105dad9085201b1f0f2
Yannic Brose <yannic@yannic-brose.de>
2025-03-17 07:42:49 +0100

    error message server error

--- app.py
+++ app.py
@@ -99,6 +99,8 @@ with gr.Blocks(theme=gr.themes.Glass(), title="BedTimeStories", css="footer{disp
         target = gr.Dropdown(label="Story for:", choices=targets)
         theme = gr.Dropdown(label="Story about:", choices=themes)
     
+    gr.Markdown("<span style="color:red"># We are currently facing connection issues with AI provider.</span>")
+
     create_button = gr.Button("Create")
     
     with gr.Row():
